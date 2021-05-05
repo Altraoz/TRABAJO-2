5a4edd2d768f19b142028f9a0270f3fa35eba29a
Altraoz <curresty@unal.edu.co>
2021-05-05 16:31:53 -0500

    Octavo commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner entrada =  new Scanner(System.in);
+        String solicitud = "-";
 
         //Inicialización del puerto
         ArrayList<LinkedList<Integer>> MatrizContenedores = new ArrayList<>();
         ArrayList<LinkedList<Integer>> MatrizAutos = new ArrayList<>();
         ArrayList<Integer> serialesA = new ArrayList<>();
+        ArrayList<Integer> serialesC = new ArrayList<>();
         int contenedores = 0, autos = 0;
 
         //Iniciar contendores
@@ -16,11 +18,11 @@ public class Main{
             for (int j = 0; j < 5; j++) {
                 if (contenedores<988){
                     MatrizContenedores.get(i).addFirst(contenedores+1);
-
-                    contenedores++;}
+                    serialesC.add(contenedores+1);
+                    contenedores++;
+                }
             }
         }
-
         //Iniciar autos
         for (int i = 0; i < 25; i++){
             MatrizAutos.add(new LinkedList<>());
@@ -32,111 +34,140 @@ public class Main{
                 }
             }
         }
-        //salidas de verificación que se deben retirar al final
+
+
         System.out.println(MatrizAutos);
         System.out.println(MatrizContenedores);
+
         //--------------------------------
-        String solicitud = "1";
+
 
         while (!solicitud.equals("0")){
             System.out.print("Ingrese su solicitud: ");
             solicitud = entrada.nextLine();
-
             //------------------------------------
             if(solicitud.equals("recibir contenedor")){
-                if (contenedores == 988){
+
+                if (contenedores == 990){
                     System.out.println("!Alerta! (Umbral alcanzado) --> No se reciben más contenedores");
                     System.out.println("------------------------------");
                     continue;}
 
+                System.out.print("Ingrese el serial del contenedor: ");
+                String ec = entrada.nextLine();
+
+                //verificamos que el serial del contenedor no se repita
+                if (serialesA.contains(Integer.valueOf(ec))){
+                    System.out.println("-Un contenedor ya tiene este serial!");
+                    System.out.println("------------------------------");
+                    continue; }
+
+                //se realiza un recorrido por todos las pilas hasta hallar una incompleto
+                //y se almacena este elemento dentro de esa pila
                 for(int i=0; i<200; i++) {
                     if (MatrizContenedores.get(i).size() < 5) {
-                        MatrizContenedores.get(i).addFirst(Integer.valueOf(entrada.nextLine()));
-                        System.out.println(180 + " segundos");
-                        System.out.println("Contenedor almacenado en la pila: " + i);
+                        MatrizContenedores.get(i).addFirst(Integer.valueOf(ec));
+                        System.out.println("-Tiempo estimado: 180 segundos");
+                        System.out.println("-Contenedor almacenado en la pila: " + (i+1));
+                        break;
                     }
                 }
-                contenedores++;
+                contenedores++; //sumamos el numero de contenedores que tenemos
             }
             //------------------------------------
-            else if (solicitud.equals("entregar contenedor")){
-                int tiempoC = 0; int indice_contenedor = 0; int aux, serial_contenedor;
-                serial_contenedor = entrada.nextInt();
+            else if (solicitud.equals("entregar contenedor")) {
 
-                //Iteración para buscar en que pila esta el elemento serial_contenedor
-                for (int i = 0; i < 200; i++) {
+                String serial_contenedor0;
+                int tiempoC = 0, indice_contenedor = 0, aa= 0, aux, serial_contenedor, i;
+                ArrayList<Integer> pil_incompletas = new ArrayList<>(), pil_usadas = new ArrayList<>();
+                System.out.print("Ingrese el serial del contenedor: ");
+                serial_contenedor0 = entrada.nextLine();
+                serial_contenedor = Integer.parseInt(serial_contenedor0);
+
+                //verificamos que el contenedor este en el listado de los seriales de contenedores
+                if (!serialesC.contains(serial_contenedor)) {
+                    System.out.println("-Automovil no encontrado");
+                    System.out.println("------------------------------");
+                    continue; }
+
+                //Iteración encargado de buscar en que pila esta el serial del contenedor
+                for (i = 0; i < 200; i++) {
                     LinkedList<Integer> pila1 = MatrizContenedores.get(i);
                     LinkedList<Integer> pila2 = new LinkedList<>();
                     aux = i;
                     while (pila1.peekFirst() != null) {
                         if (!pila1.peekFirst().equals(serial_contenedor)) {
-                            pila2.addFirst(pila1.peekFirst());
-                            pila1.pollFirst();
-                        }else {
-                            indice_contenedor = i;
+                            pila2.add(pila1.peekFirst()); }
+                        else { indice_contenedor = i;
                             i = 200;
-                            pila2.addFirst(pila1.peekFirst());
-                            pila1.pollFirst();
-                        }
-                    }
+                            pila2.add(pila1.peekFirst()); }
+                        pila1.pollFirst(); }
                     MatrizContenedores.set(aux, pila2);
                 }
 
-                //Iteracion para encontrar las pilas que esten incompletas
-                ArrayList<Integer> pil_incompletas = new ArrayList<>();
-                int pila_incompleta = 0;
-                int aa = 0;
-                for (int i = 0; i < 200; i++) {
-                    if (MatrizContenedores.get(i).size() != 5) {
-                        pil_incompletas.add(i);
-                    }
-                }
+                //Iteracion para encontrar las pilas que esten incompletas y almacenarlas en una lista
+                for (i = 0; i < 200; i++) {
+                    if (MatrizContenedores.get(i).size() != 5) { pil_incompletas.add(i); } }
 
-                //bucle que se encarga de acomodar la lista hasta que el proximo elemento de una pila sea el necesitado
+                //bucle encargado de almacenar en otras pilas los contenedores mientras el ultimo elemento
+                //no sea igual al serial del contenedor dado.
                 while (MatrizContenedores.get(indice_contenedor).peekFirst() != serial_contenedor) {
-                    if (MatrizContenedores.get(pil_incompletas.get(aa)).size() < 6){
-                        if (MatrizContenedores.get(pil_incompletas.get(aa)).size() < 5) {
-                            MatrizContenedores.get(pil_incompletas.get(aa)).addFirst(MatrizContenedores.get(indice_contenedor).peekFirst());
-                            MatrizContenedores.get(indice_contenedor).pollFirst();
-                            tiempoC += 60;
+                    if (MatrizContenedores.get(pil_incompletas.get(aa)).size() < 5) {
+                        if (!pil_usadas.contains(pil_incompletas.get(aa))) {
+                            pil_usadas.add(pil_incompletas.get(aa));
                         }
-                        else {
-                            MatrizContenedores.get(pil_incompletas.get(aa)).addFirst(MatrizContenedores.get(indice_contenedor).peekFirst());
-                            MatrizContenedores.get(indice_contenedor).pollFirst();
-                            MatrizContenedores.get(indice_contenedor).addFirst(MatrizContenedores.get(pil_incompletas.get(aa)).peekFirst());
-                            MatrizContenedores.get(pil_incompletas.get(aa)).pollFirst();
-                            tiempoC += 120;
+                        MatrizContenedores.get(pil_incompletas.get(aa)).addFirst(MatrizContenedores.get(indice_contenedor).peekFirst());
+                        MatrizContenedores.get(indice_contenedor).pollFirst();
+                        tiempoC += 60;
+                    }else{ aa++; }
+                }
+                //eliminamos el ultimo elemento de la pila (el serial del contenedor)
+                // aumentamos tiempo y restamos el numero de contenedores
+                MatrizContenedores.get(indice_contenedor).pollFirst(); tiempoC += 240; contenedores --;
+                serialesC.remove(Integer.valueOf(serial_contenedor)); //Borramos el serial de la lista
+
+                System.out.println("-Elemento almacenado en la pila: " + (indice_contenedor+1));
+                System.out.println("-Tiempo estimado: "+ tiempoC +" segundos");
+
+                //imprime las los indices de las pilas si se almacenaron contendores en otras pilas
+                if (!pil_usadas.isEmpty()){
+                    if (pil_usadas.size() == 1){
+                        System.out.print("-Contenedores reubicados en la pila: ");
+                        System.out.println(pil_usadas.get(0));
+                    }else {
+                        System.out.print("-Contenedores reubicados en las pilas: ");
+                        for (i = 0; i < pil_usadas.size(); i++) {
+                            if (i == pil_usadas.size() - 1) {
+                                System.out.println(pil_usadas.get(i) + ".");
+                            } else { System.out.print(pil_usadas.get(i)+", "); }
                         }
                     }
-                    else{ aa++; }
                 }
-
-                MatrizContenedores.get(indice_contenedor).pollFirst();
-                tiempoC += 420;
-
-                System.out.println("Elemento almacenado en la pila: " + indice_contenedor);
-                System.out.println("Tiempo estimado: "+ tiempoC +" segundos");
-                contenedores --;
-                solicitud = entrada.nextLine();
-
             }
             //------------------------------------
             else if(solicitud.equals("recibir auto")){
+
+                //verifica que no se ha llegado al umbral
                 if (autos == 371){
                     System.out.println("!Alerta! (Umbral alcanzado) --> No se reciben más autos");
                     System.out.println("------------------------------");
                     continue;}
+
                 System.out.print("Ingrese el serial del auto: ");
                 String of = entrada.nextLine();
+
+                //verifica que el serial no exista en la lista de seriales de autos
                 if (serialesA.contains(Integer.valueOf(of))){
                     System.out.println("-Un automóvil ya contiene este serial!");
                     System.out.println("------------------------------");
                     continue; }
+
+                //busca una cola que no este llena, agrega el serial e imprime los requisitos
                 for (int i = 0; i < 25; i++){
                     if (MatrizAutos.get(i).size() < 15){
                         MatrizAutos.get(i).add(Integer.valueOf(of));
                         serialesA.add(Integer.valueOf(of));
-                        System.out.println(120 + " segundos");
+                        System.out.println("-Tiempo estimado: 120 segundos");
                         System.out.println("-Auto almacenado en la cola: " + (i + 1));
                         break;
                     }
@@ -146,18 +177,20 @@ public class Main{
             //------------------------------------
             else if(solicitud.equals("entregar auto")){
 
-                int tiempo = 0; int indice_auto = 0; int aux, serial_auto;
+                int tiempo = 0, indice_auto = 0, aux, serial_auto, aa = 0;
                 String serial_autoo;
+                ArrayList<Integer> col_incompletas = new ArrayList<>(), colas_usadas = new ArrayList<>();
+
+
                 System.out.print("Ingrese el serial del auto: ");
                 serial_autoo = entrada.nextLine();
-
                 serial_auto = Integer.parseInt(serial_autoo);
                 if (!serialesA.contains(serial_auto)){
                     System.out.println("-Automóvil no encontrado");
                     System.out.println("------------------------------");
                     continue;
                 }
-                serialesA.remove(Integer.valueOf(serial_auto));
+
                 //Iteración para buscar en que cola esta el elemento serial_auto
                 for (int i = 0; i < 25; i++) {
                     LinkedList<Integer> cola1 = MatrizAutos.get(i);
@@ -173,23 +206,21 @@ public class Main{
                             cola2.add(cola1.peek());
                             cola1.poll();
                         }
-                    }
-                    MatrizAutos.set(aux, cola2);
+                    } MatrizAutos.set(aux, cola2);
                 }
+
                 //Iteracion para encontrar las colas que esten incompletas
-                ArrayList<Integer> col_incompletas = new ArrayList<>();
-                int cola_incompleta = 0;
-                int aa = 0;
                 for (int i = 0; i < 25; i++) {
                     if (MatrizAutos.get(i).size() != 15) {
                         col_incompletas.add(i);
                     }
                 }
-                ArrayList<Integer> colas_usadas = new ArrayList<>();
 
-                //bucle que se encarga de acomodar la lista hasta que el proximo elemento de una cola sea el necesitado
+                //bucle encargado de ubicar autos en otras colas hasta que
+                // el proximo elemento de una cola sea el necesitado
                 while (MatrizAutos.get(indice_auto).peek() != serial_auto) {
                     if (MatrizAutos.get(col_incompletas.get(aa)).size() < 15){
+                        //Si la cola incompleta aun no esta completa, encole el auto en esta cola
                         if (!colas_usadas.contains(col_incompletas.get(aa))){
                             colas_usadas.add(col_incompletas.get(aa));
                         }
@@ -197,25 +228,30 @@ public class Main{
                         MatrizAutos.get(indice_auto).poll();
                         tiempo += 20;
                     }else if (aa == col_incompletas.size()-1){
-
+                        //si se han lleno todas las colas, se encolan los elementos de la ultima cola en llenar
+                        //en la cola donde se encuentra el auto, y viceversa;
                         MatrizAutos.get(indice_auto).add(MatrizAutos.get(col_incompletas.get(aa)).peek());
                         MatrizAutos.get(col_incompletas.get(aa)).poll();
                         MatrizAutos.get(col_incompletas.get(aa)).add(MatrizAutos.get(indice_auto).peek());
                         MatrizAutos.get(indice_auto).poll();
                         tiempo += 40;
-                    } else{ aa++; }
+
+                    } else{ aa++;} //si la cola esta completa, encole en la proxima cola incompleta
                 }
-                MatrizAutos.get(indice_auto).poll();
-                tiempo += 140;
+                MatrizAutos.get(indice_auto).poll();// se desencola el auto
+                serialesA.remove(Integer.valueOf(serial_auto));// se retira el serial del listado de seriales de autos
+                tiempo += 140; autos --;
 
                 System.out.println("-Elemento almacenado en la cola: " + (indice_auto + 1));
                 System.out.println("-Tiempo estimado: "+tiempo+" segundos");
+
+                //si se ha requerido mover autos a otras colas se imprime los indices de las colas
                 if (!colas_usadas.isEmpty()){
                     if (colas_usadas.size() == 1){
-                        System.out.print("-Autos ubicados en la cola: ");
+                        System.out.print("-Autos reubicados en la cola: ");
                         System.out.println(colas_usadas.get(0));
                     }else {
-                        System.out.print("-Autos ubicados en las colas: ");
+                        System.out.print("-Autos reubicados en las colas: ");
                         for (int j = 0; j < colas_usadas.size(); j++) {
                             if (j == colas_usadas.size() - 1) {
                                 System.out.println(colas_usadas.get(j) + ".");
@@ -223,9 +259,8 @@ public class Main{
                         }
                     }
                 }
-                autos --;
-
             }
+
             //-------------------------------------------
             else if(solicitud.equals("capacidad")){
                 System.out.println("-Contenedores: " + contenedores);
@@ -272,7 +307,7 @@ public class Main{
             }
             //-------------------------------------------
 
-            else if(solicitud.equals("0")){System.out.println("-Fin del programa ;)");}
+            else if(solicitud.equals("0")){System.out.println("-Fin del programa _o7");}
 
             else{ System.out.println("-Entrada no valida"); }
 
